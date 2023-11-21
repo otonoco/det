19f85e34a4e11981229b71c006ef519f9438ebe3
mercurylau <lstmercury@outlook.com>
2023-11-20 23:13:20 -0600

    minor

--- pytracking/evaluation/local.py
+++ pytracking/evaluation/local.py
@@ -4,22 +4,23 @@ def local_env_settings():
     settings = EnvSettings()
 
     # Set your local paths here.
+    /mnt/data0-nfs/sl53/det/CDTB/seqscd 
 
-    settings.cdtb_path = '/home/yan/Data2/DOT-results/CDTB/sequences/'
-    settings.cdtb_st_path = '/home/yan/Data2/DOT-results/CDTB-ST/sequences/'
+    settings.cdtb_path = '/mnt/data0-nfs/sl53/det/results/CDTB/seqs'
+    settings.cdtb_st_path = '/mnt/data0-nfs/sl53/det/results/CDTB-ST/seqs'
     settings.davis_dir = ''
-    settings.depthtrack_path = '/home/yan/Data4/Datasets/DepthTrack/test/'
-    settings.depthtrack_st_path = '/home/yan/Data2/DOT-results/DepthTrack-ST/sequences/'
+    settings.depthtrack_path = '/mnt/data0-nfs/sl53/det_dataset/test'
+    settings.depthtrack_st_path = '/mnt/data0-nfs/sl53/det/results/DepthTrack-ST/sequences/'
     settings.got10k_path = ''
     settings.got_packed_results_path = ''
     settings.got_reports_path = ''
     settings.lasot_path = ''
-    settings.network_path = '/home/yan/Data2/DeT-models/'    # Where tracking networks are stored.
+    settings.network_path = '/mnt/data0-nfs/sl53/det/pytracking/networks'    # Where tracking networks are stored.
     settings.nfs_path = ''
     settings.otb_path = ''
-    settings.result_plot_path = '/home/yan/Data2/DeT-Results/pytracking/result_plots/'
-    settings.results_path = '/home/yan/Data2/DeT-Results/tracking_results/'   # Where to store tracking results
-    settings.segmentation_path = '/home/yan/Data2/DeT-Results/pytracking/segmentation_results/'
+    settings.result_plot_path = '//mnt/data0-nfs/sl53/det/results/pytracking/result_plots/'
+    settings.results_path = '/mnt/data0-nfs/sl53/det/results/tracking_results/'   # Where to store tracking results
+    settings.segmentation_path = '/mnt/data0-nfs/sl53/det/results/segmentation_results/'
     settings.tn_packed_results_path = ''
     settings.tpl_path = ''
     settings.trackingnet_path = ''
